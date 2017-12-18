06b04d3bebb38a8c258718879653c95543a5ac05
Matthew Miecnikowski <mtmiec@gmail.com>
2017-12-18 13:41:11 -0800

    removed print statements form coeff matrix calculation

--- li.f90
+++ li.f90
@@ -161,10 +161,10 @@ subroutine initialize
       if ((zflow /= 1) .and. kj==0) coeff(i,j)=0.
       ! isolate 1,1 and 2,0 if isolate == 1
       if ((isolate == 1) .and. (.not.(((ki == 1).and.(kj == 1)) .or. ((ki == 2).and.(kj == 0))))) coeff(i,j)=0.
-      if (myid==0) then
-        print*,'i = ',i,', j = ',j
-        print*,'coeff =',coeff(i,j)
-      end if
+      !if (myid==0) then
+      !  print*,'i = ',i,', j = ',j
+      !  print*,'coeff =',coeff(i,j)
+      !end if
     end do
   end do
 
