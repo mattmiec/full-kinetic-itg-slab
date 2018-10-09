4b44942b74cd84ea36c386ccfd5c2c18bf1a99f4
mtmiec <mtmiec@gmail.com>
2018-10-09 10:41:14 -0600

    merged output/diagnostics from dfe

--- li.f90
+++ li.f90
@@ -24,6 +24,9 @@ program li
   call accumulate
   call field
 
+  tstep = 0
+  call output
+
   !main loop
   do tstep=1,nt
 
@@ -49,21 +52,25 @@ program li
     call update
 
     !output
-    call modeout(phihist,'phist',11)
-    call modeout(denhist,'dhist',12)
-    call modeout(temphist,'thist',13)
-    if (mod(tstep,nrec).eq.0) then
-      call gridout(phi,'phixy',14)
-      if (dke /= 1) then
-        call gridout(den,'denxy',15)
-      else
-        call gridout(deni,'denii',17)
-        call gridout(dene,'denee',18)
-      end if
-      call gridout(tempxy,'temxy',16)
-      call gendiagnostics
-      !call ionvelocity
-    end if
+
+    ! modeout, gridout, diagnostics
+    call output
+
+!   call modeout(phihist,'phist',11)
+!   call modeout(denhist,'dhist',12)
+!   call modeout(temphist,'thist',13)
+!   if (mod(tstep,nrec).eq.0) then
+!     call gridout(phi,'phixy',14)
+!     if (dke /= 1) then
+!       call gridout(den,'denxy',15)
+!     else
+!       call gridout(deni,'denii',17)
+!       call gridout(dene,'denee',18)
+!     end if
+!     call gridout(tempxy,'temxy',16)
+!     call gendiagnostics
+!     !call ionvelocity
+!   end if
 
   end do
 
@@ -789,135 +796,442 @@ end
 !--------output subroutines---------------------------------------------
 !-----------------------------------------------------------------------
 
-subroutine modeout(hist,fl,id)
+  ! OUTPUT SUBROUTINES
 
-  !record components of phi given by modehist
+  subroutine output
 
-  implicit none
-  integer :: i,id
-  character*5 :: fl
-  character*70 :: flnm
-  complex(8) :: hist(1:nmode)
+    implicit none
 
-  if (myid==0) then
-    flnm=fl//'.out'
-    open(id,file=flnm,form='formatted',status='unknown',&
-      position='append')
-    write(id,'(f8.2)',advance="no") dt*tstep
-    do i=1,nmode
-      write(id,'(a2,e13.6,a2,e13.6)',advance="no") '  ',real(hist(i)),&
-        '  ',imag(hist(i))
+    ! selected modes of potential and density
+    call mode_output(phihist, 'phi_hist.out')
+    call mode_output(denhist, 'den_hist.out')
+    call mode_output(denhist, 'temp_hist.out')
+
+    ! various diagnostic quantities
+    if (mod(tstep, nrec) == 0) call diagnostics
+
+    ! grid quantities
+    if (mod(tstep, nrec) == 0) then
+      call grid_output(phi, 'phi_grid.out')
+      !call grid_output(phi_kmod, 'phi_kmod_grid.out')
+      call grid_output(den, 'den_grid.out')
+      if (dke == 1) then
+        call grid_output(deni, 'denii_grid.out')
+        call grid_output(dene, 'denee_grid.out')
+      end if
+      call grid_output(tempxy, 'temp_grid.out')
+    end if
+
+  end subroutine output
+
+
+  subroutine mode_output(hist, filename)
+    ! record real, imaginary components given by hist
+
+    implicit none
+    integer :: i, id = 99
+    complex(8) :: hist(1: nmode)
+    character(*) :: filename
+
+    if (myid == 0) then ! master writes to files
+
+      open(id, file = filename, form = 'formatted', status = 'unknown', position = 'append')
+
+      if (tstep == 0) then
+          write(id, '(a12, a4)', advance = 'no') 't', '    '
+          do i = 1, nmode
+            write(id, '(a4, i3.3, a1, i3.3, a1, a4)', advance = 'no') 'Re[', modeindices(1,i), ',', modeindices(2,i), ']', '    '
+            write(id, '(a4, i3.3, a1, i3.3, a1, a4)', advance = 'no') 'Im[', modeindices(1,i), ',', modeindices(2,i), ']', '    '
+          end do
+          write(id, *)
+      end if
+
+      write(id, '(f12.4)', advance = 'no') dt * tstep
+      do i = 1, nmode
+        write(id, '(a4, e12.5)', advance = 'no') '    ', real(hist(i))
+        write(id, '(a4, e12.5)', advance = 'no') '    ', imag(hist(i))
+      end do
+      write(id, *)
+
+      endfile id
+      close(id)
+
+    endif
+
+  end subroutine mode_output
+
+
+  subroutine grid_output(grid_array, filename)
+    ! record all values of grid quantity
+
+    implicit none
+    integer :: i, j, id = 99
+    real(8) :: grid_array(0: nx - 1, 0: ny - 1)
+    character(*) :: filename
+
+    if (myid == 0) then ! master writes to files
+
+      open(id, file = filename, form = 'formatted', status = 'unknown', position = 'append')
+
+      do j = 0, ny - 1
+        do i = 0, nx - 1
+          write(id, '(e12.5)') grid_array(i, j)
+        end do
+      end do
+
+      write(id, *)
+
+      endfile id
+      close(id)
+
+    endif
+
+  end subroutine grid_output
+
+
+  subroutine diagnostics
+    ! calculate and record various diagnostic quantities
+
+    implicit none
+    integer :: i, j, m, id = 99
+    real(8) :: qxi, my_qxi ! heat flux in x - direction
+    real(8) :: qxe, my_qxe ! heat flux in x - direction
+    real(8) :: wi, my_wi, w2i, my_w2i ! mean weight, mean squared weight
+    real(8) :: we, my_we, w2e, my_w2e ! mean weight, mean squared weight
+    real(8) :: my_kei, kei, fe ! kinetic and field energies
+    real(8) :: my_kee, kee ! kinetic and field energies
+    real(8) :: te
+    character(72) :: filename
+
+    qxi = 0.0
+    wi  = 0.0
+    w2i = 0.0
+    kei = 0.0
+
+    my_qxi = 0.0
+    my_wi  = 0.0
+    my_w2i = 0.0
+    my_kei = 0.0
+
+    !!$acc parallel loop reduction(+:my_qxi,my_wi,my_w2i,my_kei)
+    do m = 1, ni
+      ! x - direction heat flux
+      my_qxi = my_qxi + 0.25 * (wi0(m) + wi1(m)) * vxi(m) &
+          * (vxi(m) ** 2 + vyi(m) ** 2 + vpari(m) ** 2)
+      ! weight sum
+      my_wi = my_wi + wi1(m)
+      ! weight squared sum
+      my_w2i = my_w2i + wi1(m) ** 2
+      ! kinetic energy
+      my_kei = my_kei + 0.25 * (wi0(m) + wi1(m)) * (vxi(m) ** 2 + vyi(m) ** 2 + vpari(m) ** 2)
     end do
-    write(id,*)
-    endfile id
-    close(id)
-  endif
 
-end
+    if (dke == 1) then
+      do m=1,ne
+        ! weight sum
+        my_we = my_we + we1(m)
+        !weight squared sum
+        my_w2e = my_w2e + we1(m)**2
+        !kinetic energy
+        my_kee = my_kee + 0.25 * (we0(m) + we1(m)) *memip*vpare(m)**2
+      end do
+    end if
 
-!-----------------------------------------------------------------------
+    call mpi_allreduce(my_qxi, qxi, 1, mpi_real8, mpi_sum, mpi_comm_world, ierr)
+    call mpi_allreduce(my_wi,  wi,  1, mpi_real8, mpi_sum, mpi_comm_world, ierr)
+    call mpi_allreduce(my_w2i, w2i, 1, mpi_real8, mpi_sum, mpi_comm_world, ierr)
+    call mpi_allreduce(my_kei, kei, 1, mpi_real8, mpi_sum, mpi_comm_world, ierr)
+    if (dke == 1) then
+        call mpi_allreduce(my_we,we,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+        call mpi_allreduce(my_w2e,w2e,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+        call mpi_allreduce(my_kee,kee,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+    end if
 
-subroutine gridout(u,fl,id)
+    qxi = qxi / dble(tni)
+    wi  = wi  / dble(tni)
+    w2i = w2i / dble(tni)
+    kei = kei / dble(tni)
 
-  !record all values of grid quantity
+    if (dke == 1) then
+      we=we/dble(tne)
+      w2e=w2e/dble(tne)
+      kee=kee/dble(tne)
+    else
+      we = 0.
+      w2e = 0.
+      kee = 0.
+    end if
 
-  implicit none
-  integer :: i,j,id
-  real(8) :: u(0:nx,0:ny)
-  character*5 :: fl
-  character*70 :: flnm
+    ! total energy
+    te = kei + kee + fe
 
-  if (myid==0) then
-    flnm=fl//'.out'
-    open(id,file=flnm,form='formatted',status='unknown',&
-      position='append')
-    do j=0,ny
-      do i=0,nx
-        write(id,'(e11.4)') u(i,j)
+
+    ! field energy
+    fe = 0.
+    do j = 0, ny - 1
+      do i = 0, nx - 1
+        fe = fe + 0.5 * (ex(i,j) ** 2 + ey(i,j) ** 2)
       end do
     end do
-    endfile id
-    close(id)
-  endif
-  
-end
 
-!-----------------------------------------------------------------------
+    fe = fe/nx/ny ! factor of 8 smaller than Scott's old GK code
 
-subroutine gendiagnostics
+    if (myid == 0) then ! master writes to files
 
-  implicit none
-  integer :: id,m,i,j
-  real(8) :: qx,myqx,w2i,myw2i,w2e,myw2e,mykei,kei,mykee,kee,te
-  character*5 :: fl
-  character*70 :: flnm
+      ! heat flux
 
-  id=89
+      filename = 'qflux.out'
+      open(id, file = filename, form = 'formatted', status = 'unknown', position = 'append')
 
-  qx=0
-  w2i=0
-  w2e=0
-  kei=0
-  kee=0
+      if (tstep == 0) then
+          write(id, '(a12, a4)', advance = 'no') 't', '    '
+          write(id, '(a12, a4)', advance = 'no') 'ion_qflux_x', '    '
+          write(id, *)
+      end if
 
-  myqx=0
-  myw2i=0
-  myw2e=0
-  mykei=0
-  mykee=0
+      write(id,   '(f12.4)', advance = 'no') dt * tstep
+      write(id, '(a4, e12.5)', advance = 'no') '    ', qxi
+      write(id, *)
 
-  do m=1,ni
-    !net ion heat flux in x-direction
-    myqx = myqx + wi1(m)*vxi(m)*(vxi(m)**2+vyi(m)**2+vpari(m)**2)
-    !weight squared sum
-    myw2i = myw2i + wi1(m)**2
-    !kinetic energy
-    mykei = mykei + 0.5*wi1(m)*(vxi(m)**2+vyi(m)**2+vpari(m)**2)
-  end do
+      endfile id
+      close(id)
 
-  if (dke == 1) then
-    do m=1,ne
-      myw2e = myw2e + we1(m)**2 !weight squared sum
-      mykee = mykee + 0.5*we1(m)*memip*vpare(m)**2 !kinetic energy
+      ! weight
+
+      filename = 'weight.out'
+      open(id, file = filename, form = 'formatted', status = 'unknown', position = 'append')
+
+      if (tstep == 0) then
+          write(id, '(a12, a4)', advance = 'no') 't', '    '
+          write(id, '(a12, a4)', advance = 'no') '<w_i>', '    '
+          write(id, '(a12, a4)', advance = 'no') '<w_i^2>', '    '
+          write(id, '(a12, a4)', advance = 'no') '<w_e>', '    '
+          write(id, '(a12, a4)', advance = 'no') '<w_e^2>', '    '
+          write(id, *)
+      end if
+
+      write(id,   '(f12.4)', advance = 'no') dt * tstep
+      write(id, '(a4, e12.5)', advance = 'no') '    ', wi
+      write(id, '(a4, e12.5)', advance = 'no') '    ', w2i
+      write(id, '(a4, e12.5)', advance = 'no') '    ', we
+      write(id, '(a4, e12.5)', advance = 'no') '    ', w2e
+      write(id, *)
+
+      endfile id
+      close(id)
+
+      ! energy
+
+      filename = 'energy.out'
+      open(id, file = filename, form = 'formatted', status = 'unknown', position = 'append')
+
+      if (tstep == 0) then
+          write(id, '(a12, a4)', advance = 'no') 't', '    '
+          write(id, '(a12, a4)', advance = 'no') 'ikinetic', '    '
+          write(id, '(a12, a4)', advance = 'no') 'ekinetic', '    '
+          write(id, '(a12, a4)', advance = 'no') 'field', '    '
+          write(id, '(a12, a4)', advance = 'no') 'total', '    '
+          write(id, *)
+      end if
+
+      write(id,    '(f12.4)', advance = 'no') dt * tstep
+      write(id, '(a4, e12.5)', advance = 'no') '    ', kei
+      write(id, '(a4, e12.5)', advance = 'no') '    ', kee
+      write(id, '(a4, e12.5)', advance = 'no') '    ', fe
+      write(id, '(a4, e12.5)', advance = 'no') '    ', te
+      write(id, *)
+
+      endfile id
+      close(id)
+
+    endif
+
+  end subroutine diagnostics
+
+
+  ! PRINT SUBROUTINES FOR DEBUGGING
+
+  subroutine print_particle_array(particle_array, name)
+
+    implicit none
+    integer :: m
+    real(8), intent(in) :: particle_array(1: ni)
+    character(*) :: name
+
+    do m = 1, ni
+      print *, name, '(', m, ') = ', particle_array(m)
     end do
-  end if
+
+    print *
+
+  end subroutine print_particle_array
 
 
-  call mpi_allreduce(myqx,qx,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
-  call mpi_allreduce(myw2i,w2i,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
-  if (dke == 1) call mpi_allreduce(myw2e,w2e,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
-  call mpi_allreduce(mykei,kei,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
-  if (dke == 1) call mpi_allreduce(mykee,kee,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+  subroutine print_grid_array(grid_array, name)
 
-  qx=qx/dble(tni)
-  w2i=w2i/dble(tni)
-  kei=kei/dble(tni)
+    implicit none
+    integer :: i, j
+    real(8), intent(in) :: grid_array(0: nx - 1, 0: ny - 1)
+    character(*) :: name
 
-  if (dke == 1) then
-    w2e=w2e/dble(tne)
-    kee=kee/dble(tne)
-  else
-    w2e = 0.
-    kee = 0.
-  end if
+    do j = 0, ny - 1
+      do i = 0, nx - 1
+        print *, name, '(', i, ',', j, ') = ', grid_array(i, j)
+      end do
+    end do
 
-  ! total energy
-  te = kei + fe
-  if (dke == 1) te = te + kee
+    print *
 
-  if (myid==0) then
-    flnm='diagn.out'
-    open(id,file=flnm,form='formatted',status='unknown', &
-      position='append')
-    if (tstep==1) write(id,'(a31)') 't  qx  w2i  w2e  kei  kee fe te'
-    write(id,'(f8.2)',advance="no") dt*tstep
-    write(id,'(a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6)') &
-      '  ',qx,'  ',w2i,'  ',w2e,'  ',kei,'  ',kee,'  ',fe,'  ',te
-    endfile id
-    close(id)
-  endif
+  end subroutine print_grid_array
 
-end
+
+  subroutine print_grid_array_dft(grid_array_dft, name)
+
+    implicit none
+    integer :: i, j
+    complex(8), intent(in) :: grid_array_dft(0: nx - 1, 0: ny - 1)
+    character(*) :: name
+
+    do j = 0, ny - 1
+      do i = 0, nx - 1
+        print *, name, '(', i, ',', j, ') = ', grid_array_dft(i, j)
+      end do
+    end do
+
+    print *
+
+  end subroutine print_grid_array_dft
+
+
+!subroutine modeout(hist,fl,id)
+!
+!  !record components of phi given by modehist
+!
+!  implicit none
+!  integer :: i,id
+!  character*5 :: fl
+!  character*70 :: flnm
+!  complex(8) :: hist(1:nmode)
+!
+!  if (myid==0) then
+!    flnm=fl//'.out'
+!    open(id,file=flnm,form='formatted',status='unknown',&
+!      position='append')
+!    write(id,'(f8.2)',advance="no") dt*tstep
+!    do i=1,nmode
+!      write(id,'(a2,e13.6,a2,e13.6)',advance="no") '  ',real(hist(i)),&
+!        '  ',imag(hist(i))
+!    end do
+!    write(id,*)
+!    endfile id
+!    close(id)
+!  endif
+!
+!end
+!
+!!-----------------------------------------------------------------------
+!
+!subroutine gridout(u,fl,id)
+!
+!  !record all values of grid quantity
+!
+!  implicit none
+!  integer :: i,j,id
+!  real(8) :: u(0:nx,0:ny)
+!  character*5 :: fl
+!  character*70 :: flnm
+!
+!  if (myid==0) then
+!    flnm=fl//'.out'
+!    open(id,file=flnm,form='formatted',status='unknown',&
+!      position='append')
+!    do j=0,ny
+!      do i=0,nx
+!        write(id,'(e11.4)') u(i,j)
+!      end do
+!    end do
+!    endfile id
+!    close(id)
+!  endif
+!  
+!end
+!
+!!-----------------------------------------------------------------------
+!
+!subroutine gendiagnostics
+!
+!  implicit none
+!  integer :: id,m,i,j
+!  real(8) :: qx,myqx,w2i,myw2i,w2e,myw2e,mykei,kei,mykee,kee,te
+!  character*5 :: fl
+!  character*70 :: flnm
+!
+!  id=89
+!
+!  qx=0
+!  w2i=0
+!  w2e=0
+!  kei=0
+!  kee=0
+!
+!  myqx=0
+!  myw2i=0
+!  myw2e=0
+!  mykei=0
+!  mykee=0
+!
+!  do m=1,ni
+!    !net ion heat flux in x-direction
+!    myqx = myqx + wi1(m)*vxi(m)*(vxi(m)**2+vyi(m)**2+vpari(m)**2)
+!    !weight squared sum
+!    myw2i = myw2i + wi1(m)**2
+!    !kinetic energy
+!    mykei = mykei + 0.5*wi1(m)*(vxi(m)**2+vyi(m)**2+vpari(m)**2)
+!  end do
+!
+!  if (dke == 1) then
+!    do m=1,ne
+!      myw2e = myw2e + we1(m)**2 !weight squared sum
+!      mykee = mykee + 0.5*we1(m)*memip*vpare(m)**2 !kinetic energy
+!    end do
+!  end if
+!
+!
+!  call mpi_allreduce(myqx,qx,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+!  call mpi_allreduce(myw2i,w2i,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+!  if (dke == 1) call mpi_allreduce(myw2e,w2e,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+!  call mpi_allreduce(mykei,kei,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+!  if (dke == 1) call mpi_allreduce(mykee,kee,1,mpi_real8,mpi_sum,mpi_comm_world,ierr)
+!
+!  qx=qx/dble(tni)
+!  w2i=w2i/dble(tni)
+!  kei=kei/dble(tni)
+!
+!  if (dke == 1) then
+!    w2e=w2e/dble(tne)
+!    kee=kee/dble(tne)
+!  else
+!    w2e = 0.
+!    kee = 0.
+!  end if
+!
+!  ! total energy
+!  te = kei + fe
+!  if (dke == 1) te = te + kee
+!
+!  if (myid==0) then
+!    flnm='diagn.out'
+!    open(id,file=flnm,form='formatted',status='unknown', &
+!      position='append')
+!    if (tstep==1) write(id,'(a31)') 't  qx  w2i  w2e  kei  kee fe te'
+!    write(id,'(f8.2)',advance="no") dt*tstep
+!    write(id,'(a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6,a2,e13.6)') &
+!      '  ',qx,'  ',w2i,'  ',w2e,'  ',kei,'  ',kee,'  ',fe,'  ',te
+!    endfile id
+!    close(id)
+!  endif
+!
+!end
 
 !-----------------------------------------------------------------------
 
